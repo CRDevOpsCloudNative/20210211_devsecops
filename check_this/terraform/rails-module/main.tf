9cf6a62d719f8d193a6a34113f5fa13e91057243
Leonardo Murillo <leonardo@murillodigital.com>
2021-02-09 17:29:42 +0000

    Eliminando regla de ssh al mundo

--- check_this/terraform/rails-module/main.tf
+++ check_this/terraform/rails-module/main.tf
@@ -48,14 +48,6 @@ resource "aws_security_group" "example_rails_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Inbound SSH from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # Outbound everything
   egress {
     from_port   = 0
